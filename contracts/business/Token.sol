676dd44d40ad4c3f7db6107c13a68f5558e176d1
Thiago Martins <thiago.fmartins@outlook.com>
2024-07-24 17:01:40 -0300

    Add  burnable feature and capping without inheritance

--- contracts/business/Token.sol
+++ contracts/business/Token.sol
@@ -1,44 +1,57 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
-import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract DeGymToken is ERC20, ERC20Capped, ERC20Permit, ERC20Votes {
+contract DeGymToken is ERC20, ERC20Burnable, AccessManaged, ERC20Permit {
+    uint256 private _cap;
+
+    event CapUpdated(uint256 newCap);
+
     constructor(
-        uint256 _maxSupply,
-        uint256 _initialSupply
+        address initialAuthority,
+        uint256 initialCap
     )
-        ERC20("DeGym Token", "DGYM")
-        ERC20Capped(_maxSupply * 10 ** decimals())
-        ERC20Permit("DeGym Token")
+        ERC20("DeGymToken", "DGYM")
+        AccessManaged(initialAuthority)
+        ERC20Permit("DeGymToken")
     {
+        require(initialCap > 0, "ERC20Capped: cap is 0");
+        _cap = initialCap * (10 ** decimals());
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function cap() public view returns (uint256) {
+        return _cap;
+    }
+
+    function setCap(uint256 newCap) public restricted {
         require(
-            _initialSupply <= _maxSupply,
-            "Initial supply cannot exceed max supply"
+            newCap >= totalSupply(),
+            "New cap must be greater than or equal to total supply"
         );
-        _mint(msg.sender, _initialSupply * 10 ** decimals());
+        _cap = newCap * (10 ** decimals());
+        emit CapUpdated(_cap);
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+    function mint(address to, uint256 amount) public restricted {
+        _mintCapped(to, amount);
     }
 
-    // The functions below are overrides required by Solidity.
+    function _mintCapped(address account, uint256 amount) internal {
+        require(totalSupply() + amount <= _cap, "ERC20Capped: cap exceeded");
+        _mint(account, amount);
+    }
 
     function _update(
         address from,
         address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
-        super._update(from, to, amount);
-    }
-
-    function nonces(
-        address owner
-    ) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
-        return super.nonces(owner);
+        uint256 value
+    ) internal override(ERC20) {
+        super._update(from, to, value);
     }
 }
