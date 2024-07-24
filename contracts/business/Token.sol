f337b7661355dba5389525eb2aa16b68b3c89ac1
Thiago Martins <thiago.fmartins@outlook.com>
2024-07-24 03:18:51 -0300

    implement the ERC20Votes extension

--- contracts/business/Token.sol
+++ contracts/business/Token.sol
@@ -1,30 +1,40 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DeGymToken is ERC20, Ownable {
-    uint256 public maxSupply;
-    uint256 public currentSupply;
-
-    constructor() ERC20("DeGym Token", "DGYM") {
-        maxSupply = 10_000_000_000 * 10 ** decimals();
-        _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
-        currentSupply = 1_000_000_000 * 10 ** decimals();
+contract DeGymToken is ERC20, ERC20Capped, ERC20Permit, ERC20Votes {
+    constructor(
+        uint256 _maxSupply,
+        uint256 _initialSupply
+    ) ERC20("DeGym Token", "DGYM") ERC20Capped(_maxSupply * 10 ** decimals()) {
+        require(
+            _initialSupply <= _maxSupply,
+            "Initial supply cannot exceed max supply"
+        );
+        _mint(msg.sender, _initialSupply * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
-        require(currentSupply + amount <= maxSupply, "Exceeds max supply");
         _mint(to, amount);
-        currentSupply += amount;
     }
-    function setMaxSupply(uint256 newMaxSupply) external onlyOwner {
-        require(
-            newMaxSupply >= totalSupply(),
-            "New max supply must be greater than or equal to current supply"
-        );
-        maxSupply = newMaxSupply;
+
+    // The functions below are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+
+    function nonces(
+        address owner
+    ) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 }
